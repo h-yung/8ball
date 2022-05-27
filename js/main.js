3b2c5c923fd02fb8f7716e78667f2c5ad4102247
Rebecca Key <rek990@gmail.com>
2022-05-27 12:38:47 -0400

    Updated answer bank.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
 
-  const question = document.querySelector("#question").value.toLowerCase().split(' ').join('%20'); //taking value from user input to input element
+  const question = document.querySelector("#question").value.toLowerCase(); //taking value from user input to input element
   const res = await fetch(`/api?subject=${question}`)  //notice since you're sending req to your own server
   const data = await res.json()
   console.log(data);
